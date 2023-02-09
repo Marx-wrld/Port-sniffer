75729245e2893858402f6ad5c85352638a8bcd3b
Marx-wrld <iongonga@kabarak.ac.ke>
2023-02-09 12:22:44 +0300

    Create the Rust Routes for our server

---
+++ rust-chatapp/src/routes.rs
@@ -0,0 +1,22 @@
+//Adding all the packages we need
+use std::time::Instant;
+use actix::*;
+use actix_files::NamedFile;
+use actix_web::{get, post, web, Error, HttpRequest, HttpResponse, Responder};
+use actix_web_actors::ws;
+use diesel::{
+    prelude::*,
+    r2d2::{self, ConnectionManager},
+};
+use serde_json::json;
+use uuid::Uuid;
+use crate::db;
+use crate::models;
+use crate::server;
+use crate::session;
+type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+//Adding a route for embedding the home page to the root url
+pub async fn index() -> impl Responder {
+    NamedFile::open_async("./static/index.html").await.unwrap()
+}
--- rust-chatapp/target/debug/deps/rust_chatapp-c835756cc8518874.d
+++ rust-chatapp/target/debug/deps/rust_chatapp-c835756cc8518874.d
@@ -1,5 +1,11 @@
-c:\Users\MARX\Desktop\Real-time-chat-app-with-Rust-and-React\rust-chatapp\target\debug\deps\rust_chatapp-c835756cc8518874.rmeta: src\main.rs
+c:\Users\MARX\Desktop\Real-time-chat-app-with-Rust-and-React\rust-chatapp\target\debug\deps\rust_chatapp-c835756cc8518874.rmeta: src\main.rs src\db.rs src\models.rs src\routes.rs src\schema.rs src\server.rs src\session.rs
 
-c:\Users\MARX\Desktop\Real-time-chat-app-with-Rust-and-React\rust-chatapp\target\debug\deps\rust_chatapp-c835756cc8518874.d: src\main.rs
+c:\Users\MARX\Desktop\Real-time-chat-app-with-Rust-and-React\rust-chatapp\target\debug\deps\rust_chatapp-c835756cc8518874.d: src\main.rs src\db.rs src\models.rs src\routes.rs src\schema.rs src\server.rs src\session.rs
 
 src\main.rs:
+src\db.rs:
+src\models.rs:
+src\routes.rs:
+src\schema.rs:
+src\server.rs:
+src\session.rs:
