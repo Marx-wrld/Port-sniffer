22f8ef689fe7b19ddf221fc2c259c0de54e22a20
Marx-wrld <iongonga@kabarak.ac.ke>
2023-02-15 17:28:07 +0300

    structs

--- rust-chatapp/src/session.rs
+++ rust-chatapp/src/session.rs
@@ -5,3 +5,29 @@
 const HEARBEET: Duration = Duration::from_secs(5);
 const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+//Creating structs to store all the data we need
+#[derive(Debug)]
+pub struct WsChatSession {
+    pub id: usize,
+    pub hb: Instant,
+    pub room: String,
+    pub name: Option<String>,
+    pub addr: Addr<server::ChatServer>,
+    pub db_pool: web::Data<DbPool>,
+}
+#[derive(PartialEq, Serialize, Deserialize)]
+pub enum ChatType {
+    TYPING,
+    TEXT,
+    CONNECT,
+    DISCONNECT,
+}
+#[derive(Serialize, Deserialize)]
+struct ChatMessage {
+    pub chat_type: ChatType,
+    pub value: Vec<String>,
+    pub room_id: String,
+    pub user_id: String,
+    pub id: usize,
+}
