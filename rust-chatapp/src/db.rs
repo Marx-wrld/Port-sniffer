4f684c761e97de8c82fbfce630d45a5581699d9e
Marx-wrld <iongonga@kabarak.ac.ke>
2023-02-17 09:41:28 +0300

    prepairing the sqlite database

--- rust-chatapp/src/db.rs
+++ rust-chatapp/src/db.rs
@@ -1,1 +1,6 @@
 //db.rs
+
+//Prepairing the database - We will use SQLite to keep things simple.
+//users: Store user data. Since we’re not implementing a full authentication system at this time, we will only save the username and phone number for now
+//rooms: Store a list of all chat rooms
+//conversations: Lists where all messages are stored in our database
