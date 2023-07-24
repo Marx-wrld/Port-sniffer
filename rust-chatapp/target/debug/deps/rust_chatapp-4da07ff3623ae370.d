0e5f5c55c95c1535b24e9fd4237e7528948bfe93
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-24 09:44:12 +0300

    App rebuild

--- rust-chatapp/target/debug/deps/rust_chatapp-4da07ff3623ae370.d
+++ rust-chatapp/target/debug/deps/rust_chatapp-4da07ff3623ae370.d
@@ -1,6 +1,6 @@
-c:\Users\MARX\Desktop\Real-time-chat-app-with-Rust-and-React\Real-time-chat-app-with-Rust-and-React\rust-chatapp\target\debug\deps\rust_chatapp-4da07ff3623ae370.rmeta: src\main.rs src\db.rs src\models.rs src\routes.rs src\schema.rs src\server.rs src\session.rs
+C:\Users\MARX\Desktop\chat-app\rust-chatapp\target\debug\deps\rust_chatapp-4da07ff3623ae370.rmeta: src\main.rs src\db.rs src\models.rs src\routes.rs src\schema.rs src\server.rs src\session.rs
 
-c:\Users\MARX\Desktop\Real-time-chat-app-with-Rust-and-React\Real-time-chat-app-with-Rust-and-React\rust-chatapp\target\debug\deps\rust_chatapp-4da07ff3623ae370.d: src\main.rs src\db.rs src\models.rs src\routes.rs src\schema.rs src\server.rs src\session.rs
+C:\Users\MARX\Desktop\chat-app\rust-chatapp\target\debug\deps\rust_chatapp-4da07ff3623ae370.d: src\main.rs src\db.rs src\models.rs src\routes.rs src\schema.rs src\server.rs src\session.rs
 
 src\main.rs:
 src\db.rs:
